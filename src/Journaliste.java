8bbbc18e52ea18dc7a943279042e4d91d93787a2
celine31 <dadou20231@gmail.com>
2020-04-08 15:15:03 +0200

    modification dans la classe Journaliste, ajout des annotations, réécriture de toString, equls et hashcode

--- src/Journaliste.java
+++ src/Journaliste.java
@@ -1,20 +1,67 @@
-public class Journaliste {
-    private Integer id;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+public  class Journaliste  implements Serializable {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String nom;
     private Integer credit;
 
-    public Journaliste( Integer id) {
-      this.id=id;
-      this.nom = nom;
-      this.credit = credit;
+  //getters et setters
+  public String getNom() {
+      return nom;
     }
 
-    public String getNom() {
-      return nom;
+    public void setNom(String nom) {
+      this.nom = nom;
     }
 
     public Integer getCredit() {
       return credit;
     }
+
+    public void setCredit(Integer credit) {
+      this.credit = credit;
+    }
+
+  public Long getId() {
+    return id;
   }
+
+  public  void setId(Long id) {
+    this.id = id;
+  }
+
+  //override string, equals et hashcode
+  @Override
+  public String toString() {
+    return "Journaliste{" +
+      "id=" + id +
+      ", nom='" + nom + '\'' +
+      ", credit=" + credit +
+      '}';
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Journaliste that = (Journaliste) o;
+    return Objects.equals(id, that.id) &&
+      Objects.equals(nom, that.nom) &&
+      Objects.equals(credit, that.credit);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, nom, credit);
+  }
+}
 
